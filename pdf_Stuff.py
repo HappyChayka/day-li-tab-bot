d5de13a17380f026252eea21abe90f5a0f84175a
HappyChayka <78676821+HappyChayka@users.noreply.github.com>
2024-02-02 18:42:40 +0300

    Delete pdf_Stuff.py

--- pdf_Stuff.py
+++
@@ -1,8 +0,0 @@
-from glob import glob
-from PIL import Image
-glob = glob("filesss/*.png")
-for f in glob:
-    with Image.open(f) as image:
-        cropped = image.rotate(-0.6, expand=True)
-        cropped = cropped.crop((221, 121, 900, 1654))
-        cropped.save(f"{f}")
