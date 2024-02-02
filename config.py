4d9efc31e9c970733fcda0381c6d0167cdd5199d
HappyChayka <78676821+HappyChayka@users.noreply.github.com>
2024-02-02 18:40:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "6436220801:AAFQC-2clHRBxmzNK9_6X57AUoCzyQwPoAM"
-STUDENTS_DB = "students.db"
-BASE_WEBHOOK_URL = 'https://d5dfdot2qpr2pihi1h34.apigw.yandexcloud.net'
-WEBHOOK_PATH = '/main-function'
-WEB_SERVER_HOST = "127.0.0.1"
-WEB_SERVER_PORT = 8080
